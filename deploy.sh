4ee26097a2166a081830c014bb0b31e4dd546795
nefer1453 <mail@adresin.com>
2026-04-13 11:56:14 +0300

    Ev Liste v12 - Full PWA & Mega Envanter

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+git init
+git add .
+git commit -m "Ev Liste v12 - Full PWA & Mega Envanter"
+git branch -M main
+git remote add origin https://github.com/nefer1453/ev_liste.git
+git push -u origin main --force
